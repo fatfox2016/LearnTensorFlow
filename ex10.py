ec57d0383a21df3d314993f57a8d16af6f423695
learnpython <wuli4444@163.com>
2016-12-22 10:50:17 +0800

    Create ex10.py

---
+++ ex10.py
@@ -0,0 +1,13 @@
+tabby_cat = "\t I'm tabbed in."
+persian_cat = "I'm split \n on a line."
+backslash_cat = "I'm \\ a \\ cat."
+fat_cat = '''
+I'll do a list:
+\t* Cat foot
+\t* Fishies
+\t* Catnip\n\t* Grass
+'''
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
